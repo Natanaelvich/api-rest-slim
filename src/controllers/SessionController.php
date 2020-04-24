3a3fa1ff72b23824694ba767460d935996f4f89e
Natanael Lima <taelima1997@gmail.com>
2020-04-24 01:32:14 -0300

    added lib token jwt and create first token

--- src/controllers/SessionController.php
+++ src/controllers/SessionController.php
@@ -2,11 +2,14 @@
 
 namespace Src\controllers;
 
+use DateTime;
+use Firebase\JWT\JWT;
 use Src\DAO\SessionDAO;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Src\DAO\UserDAO;
 use Src\Models\User;
+use Tuupola\Middleware\JwtAuthentication;
 
 class SessionController
 {
@@ -22,26 +25,36 @@ class SessionController
         $password_user = $data['password_user'];
 
         $userDao  = new UserDAO;
-        $user = $userDao->findByEmail($email);
+        $user = $userDao->findByEmail($email)[0];
 
         ##verify email and password user
         if (count($user) == 0) {
             $res->getBody()->write(json_encode(["mgs" => "fails in login"]));
-            return $res->withHeader('Content-Type', 'application/json');
+            return $res->withStatus(401)->withHeader('Content-Type', 'application/json');
         }
 
-        if (sha1($password_user) != $user[0]['password_user']) {
+        if (sha1($password_user) != $user['password_user']) {
             $res->getBody()->write(json_encode(["mgs" => "password incorrect"]));
-            return $res->withHeader('Content-Type', 'application/json');
+            return $res->withStatus(401)->withHeader('Content-Type', 'application/json');
         }
 
         ##create token jwt for user
         $sessionDao = new SessionDAO;
         $token = $sessionDao->create($email, $password_user);
 
-        $token = json_encode($token);
+        $expiredAt = (new DateTime())->modify('+2 days')->format('Y-m-d H:i:s');
+        $tokenPayload = [
+            'sub' => $user['id'],
+            'name' => $user['name_user'],
+            'email' => $user['email'],
+            'expired_at' => $expiredAt
+        ];
+        $secret = sha1('natanaelima');
 
-        $res->getBody()->write($token);
+        $token = JWT::encode($tokenPayload, $secret);
+
+        #response token user
+        $res->getBody()->write(json_encode(["token" => $token, "user" => $user]));
         return $res->withHeader('Content-Type', 'application/json');
     }
 }
